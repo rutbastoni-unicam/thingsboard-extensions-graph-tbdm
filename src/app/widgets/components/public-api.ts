e5c0def8c4d4d1432557b43842b5890e9a9fbdf2
Rut Bastoni <rut.bastoni@gmail.com>
2024-07-13 16:48:47 +0200

    Public api export

--- src/app/widgets/components/public-api.ts
+++ src/app/widgets/components/public-api.ts
@@ -6,3 +6,4 @@ export * from './alarm/public-api';
 export * from './entity/public-api';
 export * from './example/public-api';
 export * from './map/public-api';
+export * from './entity/public-api';
